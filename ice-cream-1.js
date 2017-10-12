f6ba6e62ad1f4aca2802cc42f463ac253eb9f81b
tsantos17 <31961885+tsantos17@users.noreply.github.com>
2017-10-12 09:05:21 -0400

    Add files via upload

--- ice-cream-1.js
+++ ice-cream-1.js
@@ -1,2 +1,11 @@
 //Set a string variable equal to a value with a prompt. 
-var flavor = prompt("Enter an Ice Cream Flavor: ")
+var flavor = prompt("Enter an Ice Cream Flavor: ");
+//Compare a value against three conditions, providing a unique response for each condition.
+if (flavor = chocolate) {
+  alert("Your flavor is +"flavor"+.");
+          else alert("Your flavor is not +"flavor"+.");
+}
+if (flavor = strawberry){
+    alert("Your flavor is +"flavor"+");
+     else alert("Your flavor is not +"flavor"+.");
+}
