6c06aaff632b549e90dc88aa9cd53d4a9b66989b
tsantos17 <31961885+tsantos17@users.noreply.github.com>
2017-10-23 10:03:45 -0400

    Add files via upload

--- ice-cream-1.js
+++ ice-cream-1.js
@@ -1,11 +1,32 @@
+//initilaize variables
+inarray = false;
+var flavors = ["chocolate","vanilla","strawberry", "coffee","pistachio"];
 //Set a string variable equal to a value with a prompt. 
-var flavor = prompt("Enter an Ice Cream Flavor: ");
-//Compare a value against three conditions, providing a unique response for each condition.
-if (flavor = chocolate) {
-  alert("Your flavor is +"flavor"+.");
-          else alert("Your flavor is not +"flavor"+.");
+//create array with all regular flavors
+var cone = [];
+flavorlist = flavors.length;
+alert("Here are our flavors: "+flavors+"");
+for(scoop=1; scoop<4; scoop++){
+	do{
+		var flavor =prompt("Enter an ice cream flavor");
+		
+		for(index = 0; index < flavorlist; index++){
+			if (flavor == flavors[index]){
+				inarray = true;
+			}
+		}
+			if (inarray == false){
+				alert(flavor+" is not a flavor.");
+			}	
+		if (flavor=="strawberry"){
+		alert("All out of flavor "+flavor);
+		inarray = false;
+		  }
+	}
+	while (inarray == false)
+alert("Here's your "+flavor+" scoop.");
+cone[scoop-1]=flavor;
 }
-if (flavor = strawberry){
-    alert("Your flavor is +"flavor"+");
-     else alert("Your flavor is not +"flavor"+.");
-}
+coneString = cone.join(' ');
+
+alert("Enjoy your cone with " +coneString.toString());
